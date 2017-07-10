28d2e82b5956a11a25fa96049588d6fe0ed1c780
Himanshu Lilha <himanshulilha@gmail.com>
2017-07-10 19:25:16 +0530

    Demo

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+// Create our app
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', function(req, res, next) {
+	res.send("Hello, this is an ansible demo");
+});
+
+app.listen(4000, function () {
+  console.log('Express server is up on port ' + 4000);
+});
